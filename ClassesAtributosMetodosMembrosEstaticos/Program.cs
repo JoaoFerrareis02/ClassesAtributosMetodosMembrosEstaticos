930063183782d6c2eaff22079e0b239884da4088
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-02-17 13:28:29 -0300

    Criado e programado o terceiro programa teste pra aprender sobre membros estáticos.

--- ClassesAtributosMetodosMembrosEstaticos/Program.cs
+++ ClassesAtributosMetodosMembrosEstaticos/Program.cs
@@ -8,39 +8,12 @@ namespace ClassesAtributosMetodosMembrosEstaticos
         static void Main(string[] args)
         {
 
-            Produto produto = new Produto();
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre os dados do produto: ");
-            Console.Write("Nome: ");
-            produto.Nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            produto.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no estoque: ");
-            produto.Quantidade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.WriteLine("Dados do produto: " + produto);
-
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int quantidade = int.Parse(Console.ReadLine());
-            produto.AdicionarProdutos(quantidade);
-
-            Console.WriteLine();
-
-            Console.WriteLine("Dados atualizados: " + produto);
-
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            quantidade = int.Parse(Console.ReadLine());
-            produto.RemoverProdutos(quantidade);
-
-            Console.WriteLine();
-
-            Console.WriteLine("Dados atualizados: " + produto);
+            Console.WriteLine("Circunferencia: "+Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: "+ Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: "+Calculadora.Pi.ToString("F2",CultureInfo.InvariantCulture));
 
         }
 
